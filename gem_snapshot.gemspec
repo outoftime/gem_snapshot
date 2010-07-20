d70f764c31241794bb73be83b19a006d984983b8
Mat Brown <mat@patch.com>
2010-07-20 13:39:36 -0400

    Initial commit
    Everything seems to be working

---
+++ gem_snapshot.gemspec
@@ -0,0 +1,16 @@
+Gem::Specification.new do |spec|
+  spec.name = 'gem_snapshot'
+  spec.version = Gem::Version.new('0.0.1')
+  spec.files = Dir.glob(File.join(File.dirname(__FILE__), 'lib', '**', '*.rb'))
+  spec.summary = 'Dump and restore snapshot of installed gems.'
+  spec.description = <<-DESC
+Gem plugin that provides two new rubygems commands, `gem dump' and
+`gem restore'. The `dump' command outputs a manifest of installed gems in YAML
+(either to a file or STDOUT), and the `restore' command reads a dump and installs
+all gems that aren't already installed.
+DESC
+  spec.author = 'Mat Brown'
+  spec.email = 'mat@patch.com'
+  spec.rubyforge_project = 'gem_snapshot'
+  spec.has_rdoc = false
+end
---
+++ lib/rubygems/commands/snapshot_command.rb
@@ -0,0 +1,70 @@
+require 'rubygems/install_update_options'
+
+class Gem::Commands::SnapshotCommand < Gem::Command
+  include Gem::InstallUpdateOptions
+
+  def initialize
+    super('snapshot', "Dump and restore snapshots of installed rubygems", :file => nil) 
+    add_option '-f', '--file=FILE', 'Snapshot file (default is stdin/stdout)' do |f, options|
+      options[:file] = f
+    end
+    add_install_update_options
+  end
+
+  def execute
+    case options[:args].first
+    when 'dump' then dump
+    when 'restore' then restore
+    else abort(usage)
+    end
+  end
+
+  def arguments
+    "ACTION          (dump|restore)"
+  end
+
+  def usage
+    "#{program_name} ACTION"
+  end
+
+  def description
+    <<TEXT
+Allows you to easily dump a snapshot of all installed gems in an environment
+and then restore the snapshot to installed gems.
+TEXT
+  end
+
+  private
+
+  def restore
+    input =
+      if options[:file] then File.open(options[:file], 'r')
+      else STDIN
+      end
+    YAML.load(input).each_pair do |gem, versions|
+      versions.each do |version|
+        installed = Gem.source_index.find_name(gem, version)
+        if installed.empty?
+          puts "Installing #{gem} #{version}"
+          installer = Gem::DependencyInstaller.new(options).install(gem, version)
+        else
+          puts "Already have #{gem} #{version}"
+        end
+      end
+    end
+    input.close
+  end
+
+  def dump
+    out =
+      if options[:file] then File.open(options[:file], 'w')
+      else STDOUT
+      end
+    specs = Hash.new { |h, k| h[k] = [] }
+    Gem.source_index.each do |name, specification|
+      specs[specification.name] << specification.version.to_s
+    end
+    out.puts specs.to_yaml
+    out.close
+  end
+end
